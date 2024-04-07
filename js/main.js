edefa03131f759c4b76fa01db37d867a730af223
helenasimina <helen.asimina.t@gmail.com>
2024-04-07 09:26:52 -0500

    add topojson.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.onload = setMap();
+
+//set up choropleth map
+function setMap(){
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [d3.csv("data/tractdatawtrains.csv"),                    
+                    d3.json("data/tracts.topojson"),                    
+                    d3.json("data/traintrackswi.topojson")                   
+                    ];    
+    Promise.all(promises).then(callback);
+
+    function callback(data){    
+        csvData = data[0];    
+        wisconsintracts = data[1];    
+        railroads = data[2];
+        console.log(csvData);
+        console.log(wisconsintracts);
+        console.log(railroads);    
+    };
+};
