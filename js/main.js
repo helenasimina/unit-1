4857c8d211a5607f4e864d124622ae97c0693412
helenasimina <helen.asimina.t@gmail.com>
2024-04-07 12:17:21 -0500

    UPDATED WITH DATA aND PROJECTED MAP

--- js/main.js
+++ js/main.js
@@ -2,11 +2,33 @@ window.onload = setMap();
 
 //set up choropleth map
 function setMap(){
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    //create Albers equal area conic projection centered on France
+    var projection = d3.geoAlbers()
+        .center([-13.16, 43.25])
+        .rotate([81, 0, 0])
+        .parallels([25.20, 45.5])
+        .scale(2216.60)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
     //use Promise.all to parallelize asynchronous data loading
-    var promises = [d3.csv("data/tractdatawtrains.csv"),                    
-                    d3.json("data/tracts.topojson"),                    
-                    d3.json("data/traintrackswi.topojson")                   
-                    ];    
+    var promises = [];    
+    promises.push(d3.csv("data/tractdatawtrains.csv")); //load attributes from csv    
+    promises.push(d3.json("data/tracts.topojson")); //load background spatial data    
+    promises.push(d3.json("data/traintrackswi.topojson")); //load choropleth spatial data    
     Promise.all(promises).then(callback);
 
     function callback(data){    
@@ -16,11 +38,24 @@ function setMap(){
         console.log(csvData);
         console.log(wisconsintracts);
         console.log(railroads);   
-        var Wisconsin = topojson.min.feature(wisconsintracts, wisconsintracts.objects.Wisconsin),
-            RailTrain = topojson.min.feature(railroads, railroads.objects.RailTrain);
-        //examine the results
-        console.log(Wisconsin);
-        console.log(RailTrain);
+        //translate europe TopoJSON
+        var Wisconsin = topojson.feature(wisconsintracts, wisconsintracts.objects.simplifiedtable),
+            RailTrain = topojson.feature(railroads, railroads.objects.traintrackswi);
+        //add Europe countries to map
+        var witracts = map.append("path")
+            .datum(Wisconsin)
+            .attr("class", "witracts")
+            .attr("d", path);
+
+        //add France regions to map
+        var railtracks = map.selectAll(".railtracks")
+            .data(RailTrain)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "railtracks " + d.properties.adm1_code;
+            })
+            .attr("d", path);
 
     };
 };
