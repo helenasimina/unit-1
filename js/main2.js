a1360d3dcaf2d1c07644781bea47bee6dcc7858b
helenasimina <helen.asimina.t@gmail.com>
2024-04-16 14:21:41 -0500

    choropleth + background Activity 10

--- js/main2.js
+++ js/main2.js
@@ -1,168 +1,205 @@
-(function(){
+(function () {
 
-    //pseudo-global variables
-    var attrArray = ["mean_totin", "mean_cars", "mean_cars", "sum_carshz", "mean_trkdm", "Point_Coun", "Percent Below Poverty Line", "Total Population", "White-Alone", "Black-Alone"]; // List of attributes to join from CSV
-    var expressed = attrArray[7]; //initial attribute
-    
-    window.onload = setMap();
+	//pseudo-global variables
+	var attrArray = ["mean_totin", "mean_cars", "mean_cars", "sum_carshz", "mean_trkdm", "Point_Coun", "Percent Below Poverty Line", "Total Population", "White-Alone", "Black-Alone"]; // List of attributes to join from CSV
+	var expressed = attrArray[3]; //initial attribute
 
-    //set up choropleth map
-    function setMap(){
-        //map frame dimensions
-        var width = 700,
-            height = 500;
+	window.onload = setMap();
 
-        //create new svg container for the map
-        var map = d3.select("body")
-            .append("svg")
-            .attr("class", "map")
-            .attr("width", width)
-            .attr("height", height);
+	//set up choropleth map
+	function setMap() {
+		//map frame dimensions
+		var width = window.innerWidth * 0.5,
+			height = 500;
 
-        //create Albers equal area conic projection centered on France
-        var projection = d3.geoAlbers()
-            .center([-8.5, 44.8])
-            .rotate([81, 0, 0])
-            .parallels([25.20, 45.5])
-            .scale(5300.60)
-            .translate([width / 2, height / 2]);
+		//create new svg container for the map
+		var map = d3.select("body")
+			.append("svg")
+			.attr("class", "map")
+			.attr("width", width)
+			.attr("height", height);
 
-        var path = d3.geoPath()
-            .projection(projection);
+		//create Albers equal area conic projection centered on France
+		var projection = d3.geoAlbers()
+			.center([-8.5, 44.8])
+			.rotate([81, 0, 0])
+			.parallels([25.20, 45.5])
+			.scale(3200.60)
+			.translate([width / 2, height / 2]);
 
-        //use Promise.all to parallelize asynchronous data loading
-        var promises = [];    
-        promises.push(d3.csv("data/tractdatawtrains.csv")); //load attributes from csv    
-        promises.push(d3.json("data/nodatatracts.topojson")); //load background spatial data    
-        promises.push(d3.json("data/traintrackswi.topojson")); //load choropleth spatial data    
-        Promise.all(promises).then(callback);
+		var path = d3.geoPath()
+			.projection(projection);
 
-        function callback(data){    
-            csvData = data[0];    
-            wisconsintracts = data[1];    
-            railroads = data[2];   
-            
-            
-            // Iterate over each feature in the TopoJSON data
-            wisconsintracts.objects.nodatatracts.geometries.forEach(function(tract) {
-                // Find the corresponding entry in the CSV data based on GEOID
-                var csvTract = csvData.find(function(csvEntry) {
-                    return csvEntry.GEOID === tract.properties.GEOID;
-                });
+		//use Promise.all to parallelize asynchronous data loading
+		var promises = [];
+		promises.push(d3.csv("data/tractdatawtrains.csv")); //load attributes from csv    
+		promises.push(d3.json("data/nodatatracts.topojson")); //load background spatial data    
+		promises.push(d3.json("data/traintrackswi.topojson")); //load choropleth spatial data
+		promises.push(d3.json("data/US_State_Boundaries.topojson")); //load choropleth spatial data    
+		Promise.all(promises).then(callback);
 
-                // If a matching entry is found, add the attributes to the TopoJSON properties
-                if (csvTract) {
-                    attrArray.forEach(function(attr) {
-                        var val = parseFloat(csvTract[attr]); // Get the attribute value from CSV
-                        tract.properties[attr] = val; // Add the attribute to TopoJSON properties
-                    });
-                }
-            });
-           
-            console.log(csvData);
-            console.log(wisconsintracts);
-            console.log(railroads);   
-            //translate europe TopoJSON
-            var Wisconsin = topojson.feature(wisconsintracts, wisconsintracts.objects.nodatatracts),
-                RailTrain = topojson.feature(railroads, railroads.objects.traintrackswi);
-            //add Europe countries to map
-            
-    // Add Wisconsin tracts to map with fill color
-            var witracts = map.selectAll(".witracts")
-                .data(Wisconsin.features)
-                .enter()    
-                .append("path")
-                .attr("class", "witracts")
-                .attr("d", path);
-            
-
-            colorScale = makeColorScale(Wisconsin.features);
-
-        
-            //add France regions to map
-            var railtracks = map.selectAll(".railtracks")
-                .data(RailTrain.features)
-                .enter()
-                .append("path")
-                .attr("class", "railtracks")
-                .attr("d", path);
-
-            // Call the makeColorScale function with the data array
-            var colorScale = makeColorScale(Wisconsin.features);
-            
-            // Call the setEnumerationUnits function
-            setEnumerationUnits(witracts, map, path, colorScale);
-        }
-    };
-
-    //function to create color scale generator
-    function makeColorScale(witractsData){
-        var colorClasses = [
-            "#D4B9DA",
-            "#C994C7",
-            "#DF65B0",
-            "#DD1C77",
-            "#980043"
-        ];
+		function callback(data) {
+			csvData = data[0];
+			wisconsintracts = data[1];
+			USBoundaries = data[3]
+			railroads = data[2];
 
 
-        //create color scale generator
-        var colorScale = d3.scaleThreshold()
-            .range(colorClasses);
+			// Iterate over each feature in the TopoJSON data
+			wisconsintracts.objects.nodatatracts.geometries.forEach(function (tract) {
+				// Find the corresponding entry in the CSV data based on GEOID
+				var csvTract = csvData.find(function (csvEntry) {
+					return csvEntry.GEOID === tract.properties.GEOID;
+				});
 
-        //build array of all values of the expressed attribute
-        var domainArray = [];
-        witractsData.forEach(function(d) {
-            var val = parseFloat(d.properties[expressed]);
-            domainArray.push(val);
-            console.log("Pushed value:", val); // Log the value being pushed into the domainArray
-        });
+				// If a matching entry is found, add the attributes to the TopoJSON properties
+				if (csvTract) {
+					attrArray.forEach(function (attr) {
+						var val = parseFloat(csvTract[attr]); // Get the attribute value from CSV
+						tract.properties[attr] = val; // Add the attribute to TopoJSON properties
+					});
+				}
+			});
 
-        // Log the domainArray before clustering
-        console.log("Domain array before clustering:", domainArray);
+			console.log(csvData);
+			console.log(wisconsintracts);
+			console.log(railroads);
+			//translate europe TopoJSON
+			var Wisconsin = topojson.feature(wisconsintracts, wisconsintracts.objects.nodatatracts).features,////
+				RailTrain = topojson.feature(railroads, railroads.objects.traintrackswi)
+				Boundaries = topojson.feature(USBoundaries, USBoundaries.objects.US_State_Boundaries);
+			//add Europe countries to map
 
-        //cluster data using ckmeans clustering algorithm to create natural breaks
-        var clusters = ss.ckmeans(domainArray, 5);
-        //reset domain array to cluster minimums
-        domainArray = clusters.map(function(d){
-            return d3.min(d);
-        });
-        //remove first value from domain array to create class breakpoints
-        domainArray.shift();
+			// Add Wisconsin tracts to map with fill color
+			// var witracts = map.selectAll(".witracts") ////
+			// 	.data(Wisconsin.features)
+			// 	.enter()
+			// 	.append("path")
+			// 	.attr("class", "witracts")
+			// 	.attr("d", path);
 
-        // Log the domainArray after clustering
-        console.log("Domain array after clustering:", domainArray);
 
-        //assign array of last 4 cluster minimums as domain
-        colorScale.domain(domainArray);
+			colorScale = makeColorScale(Wisconsin);////
 
-        // Log the final domain set for the color scale
-        console.log("Color scale domain:", colorScale.domain());
+			var USBoundaries = map.selectAll(".USBoundaries")
+				.data(Boundaries.features)
+				.enter()
+				.append("path")
+				.attr("class", "USBoundaries")
+				.attr("d", path);
 
-        return colorScale;
-    };
+			//add France regions to map
+			var railtracks = map.selectAll(".railtracks")
+				.data(RailTrain.features)
+				.enter()
+				.append("path")
+				.attr("class", "railtracks")
+				.attr("d", path);
 
-    function setEnumerationUnits(wisconsintracts,map,path,colorScale){    
-        //add wi tracts to map   
+			// Call the makeColorScale function with the data array
+			var colorScale = makeColorScale(Wisconsin);////
 
-        var newunit = map.selectAll(".witracts")        
-            .data(wisconsintracts)        
-            .enter()        
-            .append("path")        
-            .attr("class", function(d){            
-                return "witracts " + d.properties.GEOID;        
-            })        
-            .attr("d", path)        
-                .style("fill", function(d){            
-                    var value = d.properties[expressed];            
-                    if(value) {                
-                        return colorScale(d.properties[expressed]);            
-                    } else {                
-                        return "#ccc";            
-                    }    
-            });
-        console.log("Census tracts appended to map:", newunit); // Check if census tracts are appended
-    }
+			// Call the setEnumerationUnits function
+			setEnumerationUnits(Wisconsin, map, path, colorScale);////
+			setChart(csvData, colorScale);
+		}
+	};
+
+	//function to create color scale generator
+	function makeColorScale(witractsData) {
+		var colorClasses = [
+			"#D4B9DA",
+			"#C994C7",
+			"#DF65B0",
+			"#DD1C77",
+			"#980043"
+		];
+
+
+		//create color scale generator
+		var colorScale = d3.scaleThreshold()
+			.range(colorClasses);
+
+		//build array of all values of the expressed attribute
+		var domainArray = [];
+		witractsData.forEach(function (d) {
+			var val = parseFloat(d.properties[expressed]);
+			domainArray.push(val);
+			console.log("Pushed value:", val); // Log the value being pushed into the domainArray
+		});
+
+		// Log the domainArray before clustering
+		console.log("Domain array before clustering:", domainArray);
+
+		//cluster data using ckmeans clustering algorithm to create natural breaks
+		var clusters = ss.ckmeans(domainArray, 5);
+		//reset domain array to cluster minimums
+		domainArray = clusters.map(function (d) {
+			return d3.min(d);
+		});
+		//remove first value from domain array to create class breakpoints
+		domainArray.shift();
+
+		// Log the domainArray after clustering
+		console.log("Domain array after clustering:", domainArray);
+
+		//assign array of last 4 cluster minimums as domain
+		colorScale.domain(domainArray);
+
+		// Log the final domain set for the color scale
+		console.log("Color scale domain:", colorScale.domain());
+
+		return colorScale;
+	};
+
+	function setEnumerationUnits(wisconsintracts, map, path, colorScale) {
+		//add wi tracts to map   
+
+		var newunit = map.selectAll(".witracts")
+			.data(wisconsintracts)
+			.enter()
+			.append("path")
+			.attr("class", function (d) {
+				return "witracts " + d.properties.GEOID;
+			})
+			.attr("d", path)
+			.style("fill", function (d) {
+				var value = d.properties[expressed];
+				if (value) {
+					return colorScale(d.properties[expressed]);
+				} else {
+					return "#ccc";
+				}
+			});
+		console.log("Census tracts appended to map:", newunit); // Check if census tracts are appended
+	}
+
+	function setChart(csvData, colorScale){
+		//chart frame dimensions
+		var chartWidth = window.innerWidth * 0.425,
+			chartHeight = 460;
+	
+		//create a second svg element to hold the bar chart
+		var chart = d3.select("body")
+			.append("svg")
+			.attr("width", chartWidth)
+			.attr("height", chartHeight)
+			.attr("class", "chart");
+		 //set bars for each province
+		var bars = chart.selectAll(".bars")
+			.data(csvData)
+			.enter()
+			.append("rect")
+			.attr("class", function(d){
+				return "bars " + d.adm1_code;})
+		 	.attr("width", chartWidth / csvData.length - 1)
+		 	.attr("x", function(d, i){
+				return i * (chartWidth / csvData.length);
+		 	})
+			.attr("height", 460)
+			.attr("y", 0);
+		
+	};
 
 })();
 
